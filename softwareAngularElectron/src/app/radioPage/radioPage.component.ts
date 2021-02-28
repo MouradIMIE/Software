2f45505ea48effd0a9a73c835b7132ae5beb809a
MouradBoukhalfi <mouradboukhalfi3@gmail.com>
2021-02-28 19:20:17 +0100

    Starting RadioPage But not finished yet

---
+++ softwareAngularElectron/src/app/radioPage/radioPage.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({ 
+    selector: 'radioPage', 
+    templateUrl: './radioPage.component.html', 
+    styleUrls: ['./radioPage.component.scss'] 
+}) 
+
+export class RadioPageComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit() : void {
+     
+    }
+
+}
